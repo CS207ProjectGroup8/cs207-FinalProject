7efddcf12f8b38b42f97ab93f316a607e6313607
jiayinlu19960224 <42975768+jiayinlu19960224@users.noreply.github.com>
2018-10-30 14:10:33 -0400

    10302018FixedSin()_Kay

--- ElementaryFunctions.py
+++ ElementaryFunctions.py
@@ -24,6 +24,34 @@ class ElementaryFunctions():
                 ##catch error if passed object is not numeric or autodiff
                 print("illegal argument. Needs to be either autodiff object or numeric value")
                 raise AttributeError
+               
+            
+       #(Version 2 below by Kay,10302018)---------------------------------------------------------
+          
+    def sin(other):
+        try:
+            ##try to find if the passed in other object is autodiff object and do
+            ##proper operation to the passed in object
+            other_val = other.val
+            other_der = {}       #! Here, we need a new dict, as if let other_der = other.der, 
+                                 #it is actually a pointer that would change other.der during calculation as well
+            sin_value,cos_for_der = math.sin(other_val), math.cos(other_val)
+            for key,derivative in other.der.items():          #Here, loop through other.der
+                other_der[key] = cos_for_der * derivative     #and build up other_der 
+            return AutoDiff(sin_value, "dummy", other_der)    #pass in other_der as updated der information
+
+        except:
+            try:
+                ##try to check if the passed in other object is numeric value
+                other_value = float(other)
+                return math.sin(other_value)
+            except:
+                ##catch error if passed object is not numeric or autodiff
+                print("illegal argument. Needs to be either autodiff object or numeric value")
+                raise AttributeError
+                
+         #-------------------------------------------------------------------------------------------       
+                
 
     def cos(self,other):
         try:
