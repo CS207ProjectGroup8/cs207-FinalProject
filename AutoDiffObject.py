033b8c5f463582f689231a981edbe512453fc099
GGEveryone <henry199371@gmail.com>
2018-11-06 20:59:59 -0500

    updated autoDiffObject

--- AutoDiffObject.py
+++ AutoDiffObject.py
@@ -1,6 +1,6 @@
 
 class AutoDiff():
-    
+
     ''' Create objects that return the value and partial derivatives of desired functions
 
     INSTANCE VARIABLES
@@ -13,9 +13,9 @@ class AutoDiff():
     - *args: only read in args[0], which is a dictionary of derivative(s);
             eg. {"x":1, "y":2} means partial derivative of x is 1 and
             partial derivative of y is 2;
-            Only situation that *args present is in the output of methods'
+            Only situation that *args present is 0in the output of methods'
             implementation return
-            
+
     EXAMPLE:
         EITHER: (created at the beginning by user)
             x = AutoDiff(3, "x")
@@ -24,7 +24,7 @@ class AutoDiff():
                 .....
                 return AutoDiff(x0*y0, "dummy", {"x":aa, "y":bb})
     '''
-    
+
     def __init__(self, val, varName, *args):
         self.val = val
         self.varName = varName
@@ -34,19 +34,19 @@ class AutoDiff():
             self.der = args[0]
 
     def __neg__(self):
-        
-        ''' Returns another AutoDiff object which is the negative of the instance of the complex class. 
+
+        ''' Returns another AutoDiff object which is the negative of the instance of the complex class.
         This is a special method.
-        
+
         RETURNS
         ========
         AutoDiff object with negative value and negative derivative of the current instance
-        
+
         NOTES
         =====
-        PRE: 
+        PRE:
              - Current isnstance of AutoDiff class
-             
+
         POST:
              - Return a new Autodiff class instance
 
@@ -57,7 +57,7 @@ class AutoDiff():
         >>> print(t.val, t.der)
         -1 {'x': -1}
         '''
-        
+
         derDict = {}      #Create a new dictionary to store updated derivative(s) information
         setSelfDer = set(self.der)      #give a set of keys, eg. set({x:1, y:2}) = set('x', 'y')
         # Store negatives of each partial derivative
@@ -67,22 +67,22 @@ class AutoDiff():
         return AutoDiff(-1* self.val, "dummy", derDict)
 
     def __mul__(self, other):
-                        
-        ''' Returns the another AutoDiff object which is the product of current AutoDiff object 
-            and another object (either AutoDiff object or float) separated by '*'. 
+
+        ''' Returns the another AutoDiff object which is the product of current AutoDiff object
+            and another object (either AutoDiff object or float) separated by '*'.
             This is a special method.
-        
+
         RETURNS
         ========
         A new instance of AutoDiff object
-        
+
         NOTES
         =====
-        PRE: 
-             - Current instance of AutoDiff class 
+        PRE:
+             - Current instance of AutoDiff class
              - EITHER: another instance of AutoDiff class
                  OR: float
-             
+
         POST:
              - Return a new Autodiff class instance
 
@@ -90,21 +90,21 @@ class AutoDiff():
         =========
         >>> a = AutoDiff(1, 'a')
         >>> b = AutoDiff(2, 'b')
-        >>> t = a * b 
+        >>> t = a * b
         >>> print(t.val)
         2
         >>> print(t.der['a'])
         2
         >>> print(t.der['b'])
         1
-        
+
         >>> a = AutoDiff(1, 'a')
         >>> b = 33
-        >>> t = a * b 
+        >>> t = a * b
         >>> print(t.val, t.der)
         33 {'a': 33}
         '''
-        
+
         try:
         #if isinstance(other, AutoDiff):
             derDict = {}      #Create a new dictionary to store updated derivative(s) information
@@ -141,22 +141,22 @@ class AutoDiff():
 
 
     def __add__(self, other):
-                        
-        ''' Returns the another AutoDiff object which is the sum of current AutoDiff object 
-            and another object (either AutoDiff object or float) separated by '+'. 
+
+        ''' Returns the another AutoDiff object which is the sum of current AutoDiff object
+            and another object (either AutoDiff object or float) separated by '+'.
             This is a special method.
-        
+
         RETURNS
         ========
         A new instance of AutoDiff object
-        
+
         NOTES
         =====
-        PRE: 
-             - Current instance of AutoDiff class 
+        PRE:
+             - Current instance of AutoDiff class
              - EITHER: another instance of AutoDiff class
                  OR: float
-             
+
         POST:
              - Return a new Autodiff class instance
 
@@ -164,21 +164,21 @@ class AutoDiff():
         =========
         >>> a = AutoDiff(1, 'a')
         >>> b = AutoDiff(2, 'b')
-        >>> t = a + b 
+        >>> t = a + b
         >>> print(t.val)
         3
         >>> print(t.der['a'])
         1
         >>> print(t.der['b'])
         1
-        
+
         >>> a = AutoDiff(1, 'a')
         >>> b = 33
-        >>> t = a + b 
+        >>> t = a + b
         >>> print(t.val, t.der)
         34 {'a': 1}
         '''
-        
+
         try:
         #if isinstance(other, AutoDiff):
             derDict = {}
@@ -205,22 +205,22 @@ class AutoDiff():
     __radd__ = __add__
 
     def __sub__(self,other):
-        
-        ''' Returns the another AutoDiff object which is the difference of current AutoDiff object 
-            and another object (either AutoDiff object or float) separated by '-'. 
+
+        ''' Returns the another AutoDiff object which is the difference of current AutoDiff object
+            and another object (either AutoDiff object or float) separated by '-'.
             This is a special method.
-        
+
         RETURNS
         ========
         A new instance of AutoDiff object
-        
+
         NOTES
         =====
-        PRE: 
-             - Current instance of AutoDiff class 
+        PRE:
+             - Current instance of AutoDiff class
              - EITHER: another instance of AutoDiff class
                  OR: float
-             
+
         POST:
              - Return a new Autodiff class instance
 
@@ -228,17 +228,17 @@ class AutoDiff():
         =========
         >>> a = AutoDiff(1, 'a')
         >>> b = AutoDiff(2, 'b')
-        >>> t = a - b 
+        >>> t = a - b
         >>> print(t.val)
         -1
         >>> print(t.der['a'])
         1
         >>> print(t.der['b'])
         -1
-        
+
         >>> a = AutoDiff(1, 'a')
         >>> b = 33
-        >>> t = a - b 
+        >>> t = a - b
         >>> print(t.val, t.der)
         -32 {'a': 1}
         '''
@@ -299,7 +299,7 @@ class AutoDiff():
             except:
                 print("illegal argument. Needs to be either autodiff object or numeric value.")
                 raise AttributeError
-                
+
 
 
 
@@ -334,8 +334,8 @@ class AutoDiff():
             except:
                 print("illegal argument. Needs to be either autodiff object or numeric value.")
                 raise AttributeError
-                
-                
+
+
 
 
 
@@ -355,7 +355,7 @@ if __name__ == "__main__":
 
     h = z/2
     print(h.val, h.der)
-    
+
     p = 2/z
     print(p.val, p.der)
 
