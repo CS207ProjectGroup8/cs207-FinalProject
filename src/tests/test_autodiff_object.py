5164ea7d2b643e7da1a4b22fbe991dae6620b674
Anthony Rentsch <anthony.rentsch@gmail.com>
2018-11-07 20:18:24 -0500

    changing repo structure to have src folder

---
+++ src/tests/test_autodiff_object.py
@@ -0,0 +1,10 @@
+# Tests for AutoDiffObject.py
+import numpy as np
+import pytest
+import sys
+sys.path.append("../source_code")
+from ElementaryFunctions import ElementaryFunctions
+import AutoDiffObject as autodiff
+
+ef = ElementaryFunctions()
+
