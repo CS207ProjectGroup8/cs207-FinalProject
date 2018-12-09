5d700dd5c5d5e828482cd550e3a5eaead3114aa0
lipikaramaswamy <31832945+lipikaramaswamy@users.noreply.github.com>
2018-12-09 16:09:08 -0500

    fucking crazy spacing fixes

--- hotAD/hotAD/ADfun.py
+++ hotAD/hotAD/ADfun.py
@@ -39,7 +39,7 @@ def J_F(F, x, H = False):              #F as a length n list, x as a length m li
         >>> print(J_F(F, [2, 3, 4])[1][0])
         [3. 4. 3.]
         >>> print(J_F(F, [2, 3, 4])[1][1])
-        [-2. -2. 1.]
+        [-2. -2.  1.]
                         
         >>> F2 = lambda x: [x[0] * 3 + x[1] * x[2] + x[3]*x[3]]
         >>> print(J_F(F2, [2, 3, 4, 8], H = True))
