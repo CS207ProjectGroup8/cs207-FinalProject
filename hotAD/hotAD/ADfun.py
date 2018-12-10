10ff29da52a504262bc1e7e8b73aa4aba740618e
lipikaramaswamy <31832945+lipikaramaswamy@users.noreply.github.com>
2018-12-09 19:46:36 -0500

    playing with gradient descent doctest

--- hotAD/hotAD/ADfun.py
+++ hotAD/hotAD/ADfun.py
@@ -244,7 +244,7 @@ def Mini(F, x, method = "quasi-newton-BFGS", criteria = 10**(-8), *args, rate = 
     2
     
     >>> Mini(F3, [1, 0.9], "gradient-descent")['x_min']
-    array([0.97345684, 0.9475103 ])
+    array([0.96727347, 0.9354844 ])
     >>> Mini(F3, [1, 0.9], "gradient-descent")['min F(x)']
     array([0.0007057])
     >>> Mini(F3, [1, 0.9], "gradient-descent")['number of iter']
