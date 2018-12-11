a503d1b3b138cc83f3ef726a14d27caefbfc21e7
Anthony Rentsch <anthony.rentsch@gmail.com>
2018-12-11 01:00:18 -0500

    another adfun change

--- hotAD/hotAD/ADfun.py
+++ hotAD/hotAD/ADfun.py
@@ -148,8 +148,8 @@ def Newton(F, x, criteria = 10**(-8)):
         True
         >>> np.isclose(Newton(F,x)['F(x_min): '][0], 0)
         True
-        >>> np.isclose(Newton(F,x)['number of iter: '], 536)
-        True
+        >>> Newton(F,x)['number of iter: ']
+        536
         
         '''    
     x_k = np.array(x)
